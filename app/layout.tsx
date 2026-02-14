25e53a63d7a440a1e2a21ff4fa33b4c8a08cc654
w` <sissypr0hyx@gmail.com>
2026-02-14 19:30:15 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer';
 export const metadata: Metadata = {
   title: {
     default: "MintzCheatz | Undetectable Spoofer & Cheats",
-    template: "%s | mindt" // vercel url
+    template: "%s | https://mintzcheatz.vercel.app/" // vercel url
   },
   description: "Best Spoofer & Cheats for your games!",
   keywords: ["hwid spoofer", "undetectable spoofer", "valorant spoofer", "eac spoofer", "gaming spoofer", "valorant cheat", "valorant cheats", "cs2 cheat", "cs2 cheats", "mintzcheatz", "spoofer", "cheats"],
@@ -15,11 +15,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: "MintzCheatz - Stay Undetected",
     description: "The most reliable Spoofer and cheats on the market. Instant delivery.",
-    url: "", // vercel url
+    url: "https://mintzcheatz.vercel.app/", // vercel url
     siteName: "MintzCheatz",
     images: [
       {
-        url: "/og-image.png",
+        url: "/mintzcheatz.jpg",
         width: 1200,
         height: 630,
         alt: "MintzCheatz Preview",
